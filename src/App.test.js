44303fc49707743f8940a6d76e553ab1bd710dbd
Bhuvafenal <bhuvafenal2807@gmail.com>
2024-02-20 19:58:40 -0500

    desinge change

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//test case
 test('renders learn react link1', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -9,5 +9,6 @@ const reportWebVitals = onPerfEntry => {
     });
   }
 };
+//get
 
 export default reportWebVitals;
